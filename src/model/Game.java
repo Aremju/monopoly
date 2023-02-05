a16d4cf87e884d0effe1369be7f0ccbe7c00294d
Julius <juliusarendt@icloud.com>
2023-02-05 15:44:32 +0100

    Moved Model-Components to packages

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,3 +1,5 @@
+package model;
+
 public class Game {
     private boolean restart;
     private final IGameLoop gameLoop;
--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,3 +1,5 @@
+package view;
+
 import javax.swing.*;
 
 public class GameView extends JPanel {
